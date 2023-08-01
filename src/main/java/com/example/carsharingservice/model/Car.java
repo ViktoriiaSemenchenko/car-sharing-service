c94e4921e18b076ed9633aeaa6caca68096978ac
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-08-01 12:53:43 +0300

    intermediate solution for entity and properties

--- src/main/java/com/example/carsharingservice/model/Car.java
+++ src/main/java/com/example/carsharingservice/model/Car.java
@@ -1,49 +1,33 @@
 package com.example.carsharingservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "cars")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String model;
     private String brand;
-    private int inventory;
+    @PositiveOrZero
+    private Integer inventory;
+    @Enumerated(value = EnumType.STRING)
+    private CarType carType;
+    @PositiveOrZero
     private BigDecimal dailyFee;
-    @Enumerated(EnumType.STRING)
-    private Type type;
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
-    @OneToMany(mappedBy = "car", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Rental> rentals;
 
-    public enum Type {
+    public enum CarType {
         SEDAN,
         SUV,
         HATCHBACK,
--- src/main/java/com/example/carsharingservice/model/Rental.java
+++ src/main/java/com/example/carsharingservice/model/Rental.java
@@ -1,31 +1,28 @@
 package com.example.carsharingservice.model;
 
-import java.time.LocalDate;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.Data;
 
+@Data
 @Entity
-@Table(name = "rentals")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode
+@Table(name = "rental")
 public class Rental {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDate rentalDate;
-    private LocalDate returnDate;
-    private LocalDate actualReturnDate;
-    private Long carId;
-    private Long userId;
+    private LocalDateTime rentalDate;
+    private LocalDateTime returnDate;
+    private LocalDateTime actualReturnDate;
+    @OneToOne(fetch = FetchType.LAZY)
+    private Car car;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
 }
