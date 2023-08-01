64d6e2fde134f1dd59b0739d8d22253110fbea1c
DVArt <dvart@hotmail.com>
2023-08-01 11:36:14 +0300

    add UserController

--- src/main/java/com/example/carsharingservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/carsharingservice/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.carsharingservice.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -24,5 +26,11 @@ public class UserServiceImpl implements UserService {
             return userRepository.save(user);
         }
         return null;
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElse(null);
     }
 }
