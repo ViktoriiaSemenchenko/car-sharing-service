d1cebee09c7ec72fbe57b48d6d37c048a380d37c
Pavlo Polezhay <pasha200417@gmail.com>
2023-08-02 00:41:38 +0300

    implemented HealthCheckController

---
+++ src/main/java/com/example/carsharingservice/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.example.carsharingservice.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Health check controller")
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    @Operation(summary = "Health check")
+    public String healthCheck() {
+        return "Ok";
+    }
+}
+
