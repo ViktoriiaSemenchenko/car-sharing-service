637a2d5940aa89e1767802e62a185fe8e866999d
valdemar <vova.malyarchuk2@gmail.com>
2023-08-01 20:03:47 +0300

    second

--- src/main/java/com/example/carsharingservice/repository/PaymentRepository.java
+++ src/main/java/com/example/carsharingservice/repository/PaymentRepository.java
@@ -2,8 +2,21 @@ package com.example.carsharingservice.repository;
 
 import com.example.carsharingservice.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    @Query("SELECT p FROM Payment p "
+            + "JOIN FETCH p.rental r "
+            + "JOIN FETCH r.user "
+            + "WHERE p.id = (:id)")
+    Payment findByRentalId(Long id);
+
+    @Query("SELECT p FROM Payment p "
+            + "JOIN FETCH p.rental r "
+            + "JOIN FETCH r.user u "
+            + "JOIN FETCH r.car c "
+            + "WHERE p.sessionId = (:sessionId)")
+    Payment findBySessionId(String sessionId);
 }
