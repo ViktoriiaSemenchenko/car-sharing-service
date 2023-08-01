0da89d12078f320c72c7183920711ed7555a6b63
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-08-01 17:21:22 +0300

    add liquibase functionality to the project

--- src/main/java/com/example/carsharingservice/repository/UserRepository.java
+++ src/main/java/com/example/carsharingservice/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.example.carsharingservice.repository;
 
 import com.example.carsharingservice.model.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByFirstName(String firstName);
+
+    Optional<User> findByEmail(String email);
 }
