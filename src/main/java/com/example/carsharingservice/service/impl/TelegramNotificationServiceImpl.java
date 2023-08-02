ad07f12da042625d5c2d6db9fcf6ead01982859a
Nikita <katkovnikitka@gmail.com>
2023-08-02 20:15:42 +0200

    update docker

--- src/main/java/com/example/carsharingservice/service/impl/TelegramNotificationServiceImpl.java
+++ src/main/java/com/example/carsharingservice/service/impl/TelegramNotificationServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.carsharingservice.service.impl;
 
 import com.example.carsharingservice.model.User;
 import com.example.carsharingservice.service.TelegramNotificationService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
@@ -9,10 +10,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.List;
-
 @Component
-public class TelegramNotificationServiceImpl extends TelegramLongPollingBot implements TelegramNotificationService {
+public class TelegramNotificationServiceImpl extends TelegramLongPollingBot
+        implements TelegramNotificationService {
     @Value("${telegram.bot.name}")
     private String botName;
     @Value("${telegram.bot.token}")
