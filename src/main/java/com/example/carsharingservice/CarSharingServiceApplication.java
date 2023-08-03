483dcf885c7f9b38e7ad0d769f737541a16a3546
DVArt <dvart@hotmail.com>
2023-08-04 00:07:24 +0300

    mappers_b9t

--- src/main/java/com/example/carsharingservice/CarSharingServiceApplication.java
+++ src/main/java/com/example/carsharingservice/CarSharingServiceApplication.java
@@ -2,12 +2,23 @@ package com.example.carsharingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
+@EnableScheduling
 public class CarSharingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarSharingServiceApplication.class, args);
     }
 
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(5);
+        scheduler.setThreadNamePrefix("scheduler-");
+        return scheduler;
+    }
 }
