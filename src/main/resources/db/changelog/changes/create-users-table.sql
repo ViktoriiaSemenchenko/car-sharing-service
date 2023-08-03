8aa3c268fb982cf05b0bf91d1b2464bc031cfa22
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-08-03 19:35:38 +0300

    Ended telegram Nazar  v9

--- src/main/resources/db/changelog/changes/create-users-table.sql
+++ src/main/resources/db/changelog/changes/create-users-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
                                      id bigint NOT NULL AUTO_INCREMENT,
-                                     telegram_id bigint DEFAULT -939766354,
                                      email varchar(255) DEFAULT NULL,
+                                     telegram_id bigint DEFAULT -939766354,
     first_name varchar(255) DEFAULT NULL,
     last_name varchar(255) DEFAULT NULL,
     password varchar(255) DEFAULT NULL,
